a274ba937176251ded01c825649007d5cf6e79ac
Dan Martí <daniel.marti93@hotmail.com>
2024-07-08 02:00:10 -0300

    Create es.lua
    Translateed from English

---
+++ locale/es.lua
@@ -0,0 +1,239 @@
+-- Spanish
+
+local es = {
+  
+  -- Mensajes básicos
+  welcomeMessage = "¡Ve a IsaacOnTwitch.com y selecciona Comenzar!",
+  luadebugMessage = "Vuelve a iniciar el juego con el parámetro --luadebug. Más en isaacontwitch.com",
+  
+  -- Eventos
+  evRichyName = "Richy",
+  evRichyDesc = "¡Sí, tengo suerte!",
+
+  evPoopName = "Caca",
+  evPoopDesc = "¿Quién la va a limpiar?",
+
+  evHellName = "Infierno",
+  evHellDesc = "Caliente y sangriento",
+
+  evSpikyName = "Puntas",
+  evSpikyDesc = "¡Ay!",
+
+  evEarthquakeName = "Terremoto",
+  evEarthquakeDesc = "¡Protégete la cabeza!",
+
+  evAngelRageName = "Ira angelical",
+  evAngelRageDesc = "¡Por todos los cielos!",
+
+  evDevilRageName = "Ira del diablo",
+  evDevilRageDesc = "¿Qué demonios?",
+
+  evRainbowRainName = "Lluvia arcoíris",
+  evRainbowRainDesc = "Lluvia de unicornios",
+
+  evRUNName = "CORRE",
+  evRUNDesc = "Corre, Forrest, ¡corre!",
+
+  evFlashJumpName = "Salto rápido",
+  evFlashJumpDesc = "Más rápido que la luz",
+
+  evEnemyRegenName = "Regeneración enemiga",
+  evEnemyRegenDesc = "Apoyo para tus enemigos",
+
+  evFlashmobName = "Flashmob",
+  evFlashmobDesc = "¡Repíte después de mí!",
+
+  evAttackOnTitanName = "Attack on Titan",
+  evAttackOnTitanDesc = "Tus enemigos han crecido",
+
+  evDiarrheaName = "Diarrea",
+  evDiarrheaDesc = "¿De qué estaba hecha la sopa?",
+
+  evBladeStormName = "Tormenta de cuchillas",
+  evBladeStormDesc = "¡Todos serán apuñalados!",
+
+  evMoreAwardsName = "Más premios",
+  evMoreAwardsDesc = "Más premios para ti",
+
+  evStanleyParableName = "Stanley Parable",
+  evStanleyParableDesc = "Stanley presiona los botones",
+
+  evSupernovaName = "Supernova",
+  evSupernovaDesc = "¡DESTRUYE EL MUNDO!",
+
+  evDDoSName = "DDoS",
+  evDDoSDesc = "Ataque de 50 Gb/seg",
+
+  evInterstellarName = "Interestelar",
+  evInterstellarDesc = "La gravedad puede cruzar dimensiones",
+  
+  evInvisibleName = "Invisible",
+  evInvisibleDesc = "Ahora no puedes verte. A diferencia de los enemigos.",
+  
+  evGoodMusicName = "Mala música",
+  evGoodMusicDesc = "Lo último que puedes escuchar",
+  
+  evStrabismusName = "Estrabismo",
+  evStrabismusDesc = "Ahora es definitivamente más difícil golpear",
+  
+  evInverseName = "Inverso",
+  evInverseDesc = "Solo cambia el gamepad",
+  
+  evSlipName = "Resbalón",
+  evSlipDesc = "Cuidado, suelo mojado",
+  
+  evNoDMGName = "Sin daño",
+  evNoDMGDesc = "Ideal para una pelea de jefes",
+  
+  evWhirlwindName = "Remolino",
+  evWhirlwindDesc = "Destruye todo a tu paso",
+  
+  evRussianHackersName = "Hackers rusos",
+  evRussianHackersDesc = "¡Yo no voté por esto!",
+  
+  evMachineGunName = "Ametralladora",
+  evMachineGunDesc = "Docenas de disparos por segundo",
+  
+  evToxicName = "Tóxico",
+  evToxicDesc = "Directamente desde el chat",
+  
+  evCrazyDoorsName = "Puertas locas",
+  evCrazyDoorsDesc = "Destino no claro",
+  
+  evTenetName = "Tenet",
+  evTenetDesc = "Rebobina el tiempo",
+  
+  evSuperhotName = "Super Hot",
+  evSuperhotDesc = "Los enemigos solo se mueven si tú te mueves",
+  
+  evSCP173Name = "SCP-173",
+  evSCP173Desc = "¡No parpadees!",
+  
+  evPointOfViewName = "Punto de vista",
+  evPointOfViewDesc = "Es hora de ver esto desde otro ángulo",
+  
+  evRadioactiveName = "Radioactivo",
+  evRadioactiveDesc = "¡Ahora con un 20% más de plutonio!",
+  
+  evRerunName = "Todo de nuevo",
+  evRerunDesc = "Es hora de empezar de nuevo",
+  
+  evSwitchTheChannelName = "Cambia de canal",
+  evSwitchTheChannelDesc = "Este programa se ha vuelto demasiado aburrido",
+  
+  evFlyingBananaName = "Banana voladora",
+  evFlyingBananaDesc = "Bananas normales pero mejor",
+  
+  evPyrosisName = "Pirosis",
+  evPyrosisDesc = "Cambia tu dieta",
+  
+  evTearsEqualsSpidersName = "Lágrimas = Arañas",
+  evTearsEqualsSpidersDesc = "Lanza arañas a todo lo que se mueva",
+  
+  evIpecacForAllName = "Ipecac para todos",
+  evIpecacForAllDesc = "...pero no para ti",
+  
+  evGhostbustersName = "Cazafantasmas",
+  evGhostbustersDesc = "Las almas de los muertos te siguen",
+  
+  evTelestepsName = "Telepasos",
+  evTelestepsDesc = "¿Es usted, Señor Cambiante?",
+  
+  evFlashName = "Flash",
+  evFlashDesc = "El niño más rápido vivo",
+  
+  evShadowClonesName = "Clones de sombra",
+  evShadowClonesDesc = "Los enemigos aprendieron un nuevo jutsu",
+  
+  evIsaacOfIsaacName = "Isaac de Isaac",
+  evIsaacOfIsaacDesc = "...y sus Isaacs",
+  
+  evStaticElectricityName = "Electricidad estática",
+  evStaticElectricityDesc = "Toda la habitación está electrificada",
+  
+  evBleedingName = "Sangrado",
+  evBleedingDesc = "Los enemigos ahora están sangrando. Solo te hace peor.",
+  
+  evParasiticInfectionName = "Infección parasitaria",
+  evParasiticInfectionDesc = "Rico mundo interior",
+  
+  evIAmLostName = "Soy the Lost",
+  evIAmLostDesc = "Te golpean, mueres. Buena suerte.",
+  
+  evDeepDarkName = "Profundamente oscuro",
+  evDeepDarkDesc = "La oscuridad lo absorbe todo",
+  
+  evBrokenLensName = "Lente roto",
+  evBrokenLensDesc = "El zoom es muy inestable",
+  
+  evFloorIsLavaName = "El suelo es lava",
+  evFloorIsLavaDesc = "¡Cuidado dónde pisas!",
+  
+  evRerollName = "Reroll",
+  evRerollDesc = "Espero que tus objetos fueran malos",
+  
+  evMatrixName = "Matrix",
+  evMatrixDesc = "Eres el elegido, %username%",
+  
+  evDangerName = "Peligro",
+  evDangerDesc = "El juego ahora es más difícil y peligroso",
+  
+  evTornPocketsName = "Bolsillos rotos",
+  evTornPocketsDesc = "Todo se derrama de ellos",
+  
+  evGravityName = "Gravedad",
+  evGravityDesc = "Vamos a apilarla",
+  
+  evAllergiaName = "Alergia",
+  evAllergiaDesc = "Este sótano está demasiado polvoriento",
+  
+  evHeavyRainName = "Lluvia intensa",
+  evHeavyRainDesc = "Presiona X para JASOOOON",
+  
+  evMarbleBallsName = "Bolas de mármol",
+  evMarbleBallsDesc = "Bonitas e inútiles",
+  
+  evWeHateYouName = "Te odiamos",
+  evWeHateYouDesc = "Literalmente todo está tratando de matarte",
+  
+  evCallToTheVoidName = "Llamada al Vacío",
+  evCallToTheVoidDesc = "Un antiguo mal está de tu lado",
+  
+  evFiveSpacesName = "Cinco espacios",
+  evFiveSpacesDesc = "¡Reserva! Cinco usos aleatorios gratis",
+  
+  evBossfightName = "Lucha de jefes",
+  evBossfightDesc = "Sí, justo ahora",
+  
+  evGiveMeYourMoneyName = "Dame tu dinero",
+  evGiveMeYourMoneyDesc = "Estás siendo perseguido por extrañas máquinas tragamonedas",
+  
+  evWallOfTearsName = "Muro de lágrimas",
+  evWallOfTearsDesc = "¡Mira mamá! ¡Como Hush!",
+  
+  evFliesAndFliesName = "Moscas y más moscas",
+  evFliesAndFliesDesc = "Porque necesitamos más moscas",
+  
+  evKillPlayerName = "Matar al jugador",
+  evKillPlayerDesc = "Ups... quise decir CURAR al jugador",
+  
+  evCursedName = "M A L D I T O",
+  evCursedDesc = "El chat maldito te ha maldecido",
+  
+  evAutoAimName = "Auto-apuntado",
+  evAutoAimDesc = "Tan avanzado que decide a quién disparar",
+  
+  evFingerSnapName = "Chasquido de dedos",
+  evFingerSnapDesc = "Las mazmorras están un 50% más limpias",
+  
+  evYouBelongToUsName = "¡Perteneces a nosotros!",
+  evYouBelongToUsDesc = "Controla al jugador desde el chat con comandos izquierda, derecha, arriba o abajo",
+  
+  evBlueScreenName = "Pantalla azul de la muerte",
+  evBlueScreenDesc = "Micronicalis se preocupa por la calidad de su producto",
+  
+  evThrowItAwayName = "Tíralo",
+  evThrowItAwayDesc = "¿Por qué los recoges en absoluto?",
+}
+
+return es
